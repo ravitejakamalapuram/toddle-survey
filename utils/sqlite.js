2517374ab2ab524783d2095bcd0419419afc24a2
Ravi teja <raviteja.kamalapuram@healthplix.com>
2020-07-07 17:08:16 +0530

    added sqlite utility

--- utils/sqlite.js
+++ utils/sqlite.js
@@ -3,11 +3,9 @@ const database = new sqlite3.Database("./my.db");
 
 const init_tables = () => {
     const query = `
-        CREATE TABLE IF NOT EXISTS contacts (
-        id integer PRIMARY KEY,
-        firstName text,
-        lastName text,
-        email text UNIQUE)`;
+        CREATE TABLE IF NOT EXISTS users (
+        username text PRIMARY KEY UNIQUE,
+        password text)`;
     return database.run(query);
 }
 init_tables();
