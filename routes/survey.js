3df84f243d1455bcb892ecb328133433c3407628
Ravi teja <raviteja.kamalapuram@healthplix.com>
2020-07-08 18:01:14 +0530

    get survey_results query

--- routes/survey.js
+++ routes/survey.js
@@ -1,59 +1,46 @@
 const router = require("express").Router();
 const graphql = require("graphql");
-const ExpressGraphQL = require("express-graphql");
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const database = require("../utils/sqlite").sqlite;
 
-// const SurveyType = new graphql.GraphQLObjectType({
-//     name: "survey",
-//     fields: {
-//         id: { type: graphql.GraphQLID },
-//         firstName: { type: graphql.GraphQLString },
-//         lastName: { type: graphql.GraphQLString },
-//         email: { type: graphql.GraphQLString }
-//     }
-// });
+const SurveyModel = new graphql.GraphQLObjectType({
+    name: "survey",
+    fields: {
+        survey_id: { type: graphql.GraphQLID },
+        q_id: { type: graphql.GraphQLString },
+        question: { type: graphql.GraphQLString },
+        true: { type: graphql.GraphQLInt },
+        false: { type: graphql.GraphQLInt }
+    }
+});
 
-// var queryType = new graphql.GraphQLObjectType({
-//     name: 'Query',
-//     fields: {
-//         contacts: {
-//             type: graphql.GraphQLList(SurveyType),
-//             resolve: (root, args, context, info) => {
-//                 return new Promise((resolve, reject) => {
+var queryType = new graphql.GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        survey_result: {
+            type: graphql.GraphQLList(SurveyModel),
+            args: {
+                survey_id: {
+                    type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+                }
+            },
+            resolve: (root, args, context, info) => {
+                return new Promise((resolve, reject) => {
+                    database.all("SELECT * FROM surveys WHERE survey_id = $survey_id;", { $survey_id: args.survey_id }, function (err, rows) {
+                        if (err) { reject([]); }
+                        resolve(rows);
+                    });
+                });
+            }
+        }
+    }
+});
 
-//                     database.all("SELECT * FROM contacts;", function (err, rows) {
-//                         if (err) {
-//                             reject([]);
-//                         }
-//                         resolve(rows);
-//                     });
-//                 });
+const schema = new graphql.GraphQLSchema({
+    query: queryType,
+    // mutation: mutationType
+});
 
-//             }
-//         },
-//         contact: {
-//             type: SurveyType,
-//             args: {
-//                 id: {
-//                     type: new graphql.GraphQLNonNull(graphql.GraphQLID)
-//                 }
-//             },
-//             resolve: (root, { id }, context, info) => {
-//                 return new Promise((resolve, reject) => {
-
-//                     database.all("SELECT * FROM contacts WHERE id = (?);", [id], function (err, rows) {
-//                         if (err) {
-//                             reject(null);
-//                         }
-//                         resolve(rows[0]);
-//                     });
-//                 });
-//             }
-//         }
-//     }
-// });
-// const schema = new graphql.GraphQLSchema({
-//     query: queryType
-// });
-// router.use("/survey", ExpressGraphQL({ schema: schema, graphiql: true }));
+router.use("/survey", graphqlHTTP({ schema: schema, graphiql: true }));
 
 module.exports = router;
